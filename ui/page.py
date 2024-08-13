7256733c0377152f208b9891199a70c428c82407
deptno <deptno@gmail.com>
2024-08-13 14:50:55 +0900

    쓰지 않는 함수 제거

--- ui/page.py
+++ ui/page.py
@@ -1,12 +1,6 @@
 import gradio as gr
-import time
 from typing import Callable, Any
 
-def echo(message, history, system_prompt, tokens):
-    response = f"System prompt: {system_prompt}\n Message: {message}."
-    for i in range(min(len(response), int(tokens))):
-        time.sleep(0.05)
-        yield response[: i + 1]
 
 def launch(handler: Callable[[Any, Any, Any, Any], str]) -> None:
     demo = gr.ChatInterface(
