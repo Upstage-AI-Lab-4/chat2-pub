cfd71ed76bce0060a722dee22623eefb097ddc6b
deptno <deptno@gmail.com>
2024-08-13 14:51:17 +0900

    feat: auto-reloading 을 위한 컨벤션 적용

--- ui/page.py
+++ ui/page.py
@@ -3,10 +3,7 @@ from typing import Callable, Any
 
 
 def launch(handler: Callable[[Any, Any, Any, Any], str]) -> None:
-    demo = gr.ChatInterface(
-        handler,
-        additional_inputs=[
-        ],
-    )
+    with gr.ChatInterface(handler, additional_inputs=[]) as demo:
+        pass
 
     demo.launch()
