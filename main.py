cf0bab6d11da05a651ae109f86c62e20f9bfb8b6
deptno <deptno@gmail.com>
2024-08-13 14:40:53 +0900

    feat: on_user_input 메서드를 통해서 모델로의 파이프라인 인터페이스 시작

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 from ui import page
 
+
+def model(input):
+    return f'''
+    TODO: model output string
+    input: {input}
+    '''
+
+
+def on_user_input(message, history, system_prompt, tokens):
+    print(f'''
+        message: {message}
+        history: {history}
+        system_prompt: {system_prompt}
+        tokens: {tokens}
+    ''')
+
+    yield model(message)
+
+
 if __name__ == '__main__':
-    page.launch()
+    page.launch(on_user_input)
