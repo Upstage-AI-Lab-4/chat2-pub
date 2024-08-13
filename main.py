bced82f179ad75e0a63c760c8a9c7a90ce3c7fa5
chosungsu <ski06043@gmail.com>
2024-08-13 17:31:03 +0900

    add: setLogger function made

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from ui import page
 from llm.upstage import chat, name
+import logging
 
+def setLogger():
+    """
+    setLogger 함수 설명
+    로그 출력을 ERROR보다 상위레벨만 출력되도록 설정하였습니다.
+    """
+    logging.basicConfig()
+    logging.getLogger('Langchain.retrievers.multi_query').setLevel(logging.ERROR)
 
 def model(input):
     return f'''
