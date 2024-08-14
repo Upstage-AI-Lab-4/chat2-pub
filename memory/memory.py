2699c85e8e4987c3baab6e9ccf564f1f65a1a917
chosungsu <ski06043@gmail.com>
2024-08-14 11:18:37 +0900

    modify: memory

--- memory/memory.py
+++ memory/memory.py
@@ -1,5 +1,11 @@
-def memory(data: [str]) -> str:
+from langchain.memory import ConversationBufferWindowMemory
+
+def save_memory():
     """
     prompt 에 주입될 persona_memory 생성
     """
-    return ''
+    return ConversationBufferWindowMemory(
+        k=20, 
+        ai_prefix='Smart Investor', 
+        human_prefix='human'
+    )
