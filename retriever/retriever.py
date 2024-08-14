73dd61d12a26b0a5e3d349094381682ff18533d1
jenny20240401 <jenny20240401@gmail.com>
2024-08-14 13:43:12 +0900

    retriever.py수정2

--- retriever/retriever.py
+++ retriever/retriever.py
@@ -1,34 +1,27 @@
+import os
 #from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_upstage import UpstageEmbeddings
-from langchain_community.vectorstores import FAISS
-import os
+
 
 class Retriever:
-    def __init__(self, api_key):
-        self.api_key = api_key
-        self.embed_model = None
-        self.vector_index = None
-        self.retriever = None
-
-    def retriever(self, documents, query, vector_index_path):
-        #self.embed_model = OpenAIEmbeddings(api_key=self.api_key, model='text-embedding-3-small')
+    def __init__(self):
+        self.api_key = "-"
+        os.environ["OPENAI_API_KEY"] = self.api_key
+        os.environ.get("OPENAI_API_KEY")
+        #self.embedding_model = OpenAIEmbeddings(api_key=self.api_key, model="text-embedding-3-small") 
+        self.embedding_model = UpstageEmbeddings(api_key=self.api_key, model="solar-embedding-1-large")
         
-        self.embed_model = UpstageEmbeddings(
-            api_key=self.api_key,
-            model="solar-embedding-1-large"
-        )
+
+    def retriever(self, vector_store, query):
+        retriever = vector_store.as_retriever(search_type="mmr", search_kwargs={"k": 3})
+        result = retriever.get_relevant_documents(query)
+
+        # for d in result:
+        #     print(d.page_content)
+        #     print("===") 
+        return result
         
-        self.vector_index = FAISS.from_documents(documents, self.embed_model)
-        self.retriever = self.vector_index.as_retriever(search_type="mmr", search_kwargs={"k": 3})
 
-        result = self.retriever.get_relevant_documents(query)
-        for doc in result:
-            print(doc.page_content)
-            print("===")
-
-        if vector_index_path:
-            self.vector_index.save_local(vector_index_path)
-
-    def run(self):
-        # retriever 함수를 호출
-        pass
+       
+    
+    
