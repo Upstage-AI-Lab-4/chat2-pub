c8bd02b921e15dd5eadd34ebf0975c3416229fdf
jenny20240401 <jenny20240401@gmail.com>
2024-08-14 11:17:20 +0900

    “retriever.py”

---
+++ retriever/retriever.py
@@ -0,0 +1,7 @@
+class Retriever:
+    def __init__(self, vector_store):
+        self.vector_store = vector_store
+
+    def run(self):
+        # RAG(Retrieval-Augmented Generation)를 수행하는 코드
+        pass
